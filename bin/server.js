67369cf2369693914964b164c716a36534d9bac0
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-11-05 02:43:37 +0700

    add redis

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,6 @@
 const app = require(`../src/app`)
 require(`dotenv`).config();
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT,() => {
   console.log(`App listening on ${PORT}`);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+import axios from "axios";
+
+const Redis = require(`redis`);
+
+const resClient = Redis.createClient();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/photos", async (req, res, next) => {
+  const albumID = req.body.albumID;
+
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/photos`,
+    { params: albumID }
+  );
+
+  res.render("index", { title: data });
 });
 
 module.exports = router;
