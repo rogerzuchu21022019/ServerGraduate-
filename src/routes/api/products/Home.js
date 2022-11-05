def596d5fe64f6731c1a243603aefe2b903c1f03
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-11-05 21:43:57 +0700

    update finish Navigation , Middleware , app ,util ,routes

--- src/routes/api/products/Home.js
+++ src/routes/api/products/Home.js
@@ -1,7 +1,12 @@
 const express = require(`express`);
 const navigation = require("../../../utils/client-web/Navigation");
 const router = express.Router();
-router.get('/home', (req, res, next) => {
-  res.render("home", { login: navigation.LOGIN, title: "namvt" });
+router.get("/home", (req, res, next) => {
+  res.render("home", {
+    home: navigation.HOME,
+    login: navigation.LOGIN,
+    logout: navigation.LOGOUT,
+    title: "namvt",
+  });
 });
 module.exports = router;
--- src/routes/api/users/auth/Login.js
+++ src/routes/api/users/auth/Login.js
@@ -3,6 +3,8 @@ const navigation = require("../../../../utils/client-web/Navigation");
 const router = express.Router();
 
 router.get("/auth-login", (req, res, next) => {
-  res.render(`login`, { title:"namvt",home: navigation.HOME });
+  res.render(`login`, { title:"namvt",home: navigation.HOME ,
+  login: navigation.LOGIN,
+  logout: navigation.LOGOUT , });
 });
 module.exports = router;
