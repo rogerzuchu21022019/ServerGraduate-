557f33aa2d384cd1c30da49f1ba74789df8dbafd
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-11-05 21:46:24 +0700

    update finish Navigation , Middleware , app ,util ,routes

--- src/routes/api/Api.User.js
+++ src/routes/api/Api.User.js
@@ -1,5 +1,5 @@
 const loginRouter = require("../api/users/auth/Login");
-const registerRouter = require("../api/users/register");
+const registerRouter = require("../api/users/Register");
 const logoutRouter = require("../api/users/auth/Logout");
 
 module.exports = {
