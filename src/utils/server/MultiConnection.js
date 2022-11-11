27a2cdfeaa70e947ebfbc206608d9768ad2fbcd8
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-11-12 03:02:43 +0700

    update

--- src/utils/server/MultiConnection.js
+++ src/utils/server/MultiConnection.js
@@ -31,11 +31,11 @@ function newConnection(uri) {
 }
 
 /* URI lấy từ .env */
-const URI_USER_MONGODB = process.env.URI_USER_MONGODB;
+const URI_MONGODB_ONL = process.env.URI_MONGODB_ONL;
 const URI_PRODUCT_MONGODB = process.env.URI_PRODUCT_MONGODB;
 
 /* Tạo connection */
-const userConnection = newConnection(URI_USER_MONGODB);
+const userConnection = newConnection(URI_MONGODB_ONL);
 const productConnection = newConnection(URI_PRODUCT_MONGODB);
 
 /* Export số lượng lớn connection */
