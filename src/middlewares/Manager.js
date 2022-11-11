ce6d514171a452a1ed552bad0866f212d2da5ce9
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-11-12 02:24:11 +0700

    update

--- src/middlewares/Manager.js
+++ src/middlewares/Manager.js
@@ -3,16 +3,32 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const express = require("express");
+const session = require(`express-session`);
+const Redis = require(`ioredis`);
+const Redis_Store = require(`connect-redis`)(session);
+const redisClient = new Redis();
+const cors = require(`cors`);
 
 const ManagerMiddleware = (app) => {
   app.set("views", path.join(__dirname, "../views"));
   app.set("view engine", "ejs");
 
+  app.use(cors());
   app.use(logger("dev"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(cookieParser());
+
+  app.use(
+    session({
+      secret: "keyboard cat",
+      store: new Redis_Store({ client: redisClient }),
+      resave: false,
+      saveUninitialized: true,
+      cookie: { secure: true, maxAge: 5 * 60 * 1000, httpOnly: true },
+    })
+  );
 
   const fixPublic = express.static(path.join(__dirname, "public"));
   app.use(fixPublic);
